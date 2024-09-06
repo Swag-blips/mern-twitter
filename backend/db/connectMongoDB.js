b05a3fff916107291c8814d2a9b137b13c3365a3
Swag-blips <coderblip@gmail.com>
2024-09-06 10:17:02 +0100

    minor changes

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectMongoDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`mongodb connectected ${JSON.parse(connection)}`);
+    console.log(`mongodb connectected ${connection}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
     ProcessingInstruction.exit(1);
