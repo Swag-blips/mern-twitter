cb0804c8ec8bd03ac70018c4daae08f6c530af77
Swag-blips <coderblip@gmail.com>
2024-09-07 11:37:10 +0100

    update route file and paths

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/auth.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
-import userRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
