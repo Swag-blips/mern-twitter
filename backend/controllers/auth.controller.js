63bb6d31e1661296da9768b4fef5d28e1ec01cc4
Swag-blips <coderblip@gmail.com>
2024-09-13 12:53:37 +0100

    init

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -92,7 +92,12 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    res.cookie("jwt", "", { maxAge: 0 });
+    res.cookie("jwt", "", {
+      maxAge: 0,
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+    });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log(error.message);
